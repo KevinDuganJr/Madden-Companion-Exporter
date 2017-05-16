161cb26d5adb7a4e97d026bc1478d0d93e4840db
Kevin <zero2cool80@gmail.com>
2017-05-15 20:14:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./REPLACE_WITH_SERIVCE_ACCOUNT.json");
+const serviceAccount = require("./dugan-760bc-firebase-adminsdk-bguij-42efe32ea8.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
+  databaseURL: "https://dugan-760bc.firebaseio.com"
 });
 
 // Setup
