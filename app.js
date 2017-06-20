7768f3e061cf620f17e77bf2cfce83e5dfbfb134
Kevin <zero2cool80@gmail.com>
2017-06-19 20:59:23 -0500

    back to original

--- app.js
+++ app.js
@@ -29,18 +29,14 @@ app.get('/', function(req, res) {
   return res.send('Madden Data')
 });
 
-app.get('/:user', function(req, res) {
-  user = req.params.user;
-  res.send("userId is set to " + req.params.user);
-});
 
 
 app.post('/:user/:platform/:leagueId/leagueteams', (req, res) => {
   const db = admin.database();
   const ref = db.ref();
   const {user, platform, leagueId} = req.params;
- // const dataRef = ref.child(`data/${platform}/${leagueId}/leagueteams`);
-  const dataRef = ref.child(`data/${user}/${platform}/${leagueId}/leagueteams`);
+  const dataRef = ref.child(`data/${platform}/${leagueId}/leagueteams`);
+  //const dataRef = ref.child(`data/${platform}/${leagueId}/leagueteams`);
   const {body: {leagueTeamInfoList}} = req;
 
   dataRef.set({
