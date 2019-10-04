caa650d23f4e2bbbcbf4ebad8d99a67d7076f9e8
Kevin <zero2cool80@gmail.com>
2019-10-04 14:13:34 -0500

    Test

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ admin.initializeApp({
 // Change the default port here if you want for local dev.
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
-app.use(bodyParser.json({limit: '50mb'}));
+
+app.use(bodyParser.json({type: 'application/json'}))
+//app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 function capitalizeFirstLetter(string) {
