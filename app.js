dd9d653a1837eadcb6edde6d81b7741ea183d67e
Kevin <zero2cool80@gmail.com>
2019-10-04 14:18:01 -0500

    .

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ admin.initializeApp({
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
 
-app.use(bodyParser.json({type: 'application/json'}))
-//app.use(bodyParser.json({limit: '50mb'}));
+//app.use(bodyParser.json({type: 'application/json'}))
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 function capitalizeFirstLetter(string) {
