b4db1745a41d4896ec6e63d5783ec56b5fce9b00
Kevin Dugan Jr <kduganjr@valleypackagingsupply.com>
2021-08-27 09:46:11 -0500

    Reverted

--- app.js
+++ app.js
@@ -16,14 +16,11 @@ admin.initializeApp({
 
 app.set('port', (process.env.PORT || 5000));
 
-// // get user 
-// app.get('/:user', function(req, res) {
-//     return res.send("username is set to " + req.params.user);
-// });
-
-app.get('*', (req, res) => {
-    res.send('Madden Companion Exporter');
+// get user 
+app.get('/:user', function(req, res) {
+    return res.send("username is set to " + req.params.user);
 });
+
 
 // delete user data
 app.get('/delete/:user', function(req, res) {
